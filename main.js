989783137962231cc0f27e098f309c0457cfaf10
ricarellano <rarellanob@gmail.com>
2017-02-19 22:32:38 -0800

    display map with polymer

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 console.log("hola");
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.get('/', function(req, res) {
+    res.sendFile('views/index.html', {
+        root: __dirname
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.port || 3000, function() {
+    console.log('listening on port 3k')
+})
